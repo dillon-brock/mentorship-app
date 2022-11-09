e6b7a2153139bdd3072c1310e85a474c11c0c928
Dillon Brock <mdillon1297@gmail.com>
2022-11-09 15:36:53 -0800

    created student model and wrote route to sign up new student

---
+++ common/studentTypes.ts
@@ -0,0 +1,17 @@
+import Teacher from "../server/models/Teacher.js";
+
+export type StudentFromDatabase = {
+  id: string;
+  user_id: string;
+  first_name: string;
+  last_name: string;
+  image_url: string;
+  teachers?: Array<Teacher>
+}
+
+export type NewStudentInfo = {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  imageUrl: string;
+}
---
+++ server/controllers/students.ts
@@ -0,0 +1,32 @@
+import { Router, type Request, type Response } from 'express';
+import Student from '../models/Student.js';
+import { UserService } from '../services/UserService.js';
+
+const ONE_DAY_IN_MS = 1000 * 60 * 60 * 24;
+
+export default Router()
+  .post('/', async (req: Request, res: Response, next: (e?: any) => any) => {
+    try {
+      const {
+        email,
+        password,
+        firstName,
+        lastName,
+        imageUrl
+      } = req.body;
+      const newUser = await UserService.create({ email, password, type: 'student' });
+      await Student.create({ userId: newUser.id, firstName, lastName, imageUrl });
+      const sessionToken = await UserService.signIn({ email, password });
+
+      res
+        .cookie(process.env.COOKIE_NAME, sessionToken, {
+          httpOnly: true,
+          secure: process.env.SECURE_COOKIES === 'true',
+          sameSite: process.env.SECURE_COOKIES === 'true' ? 'none' : 'strict',
+          maxAge: ONE_DAY_IN_MS,
+        })
+        .json({ message: 'Signed in successfully!' });
+    } catch (e) {
+      next(e);
+    }
+  })
