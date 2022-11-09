80fda88db42dceba5f32ead7b650175287ff967f
Dillon Brock <mdillon1297@gmail.com>
2022-11-09 12:32:21 -0800

    installed react-bootstrap, removed placeholder files in /client

--- client/app.tsx
+++ client/app.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './reset.css';
 // import './global.css';
-import fooListFn from './components/foo-list'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-const FooList = fooListFn()
 const container = document.getElementById('app') || document.createElement('div')
 container.id = 'app'
 const root = createRoot(container);
@@ -13,7 +11,6 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="foos" element={ <FooList /> } />
       </Routes>
     </Router>
   </React.StrictMode>
