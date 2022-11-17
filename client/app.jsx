d433f1475b92988708f8a3b1abea7d04a8ede74c
Dillon Brock <mdillon1297@gmail.com>
2022-11-17 00:10:07 -0800

    converted chat inbox to functional component, returned to react strict mode

--- client/app.jsx
+++ client/app.jsx
@@ -6,30 +6,24 @@ import ChatInbox from './components/ChatInbox/ChatInbox';
 import StudentListPage from './components/StudentListPage/StudentListPage';
 import TeacherDetailPage from './components/TeacherDetailPage/TeacherDetailPage';
 import TeacherSearchPage from './components/TeacherSearchPage/TeacherSearchPage';
-import { UserProvider, useUserContext } from './context/UserContext';
-
-function RouteList() {
-  const { user } = useUserContext();
-  
-  return (
-    <Routes>
-      <Route path="/auth/:method" element={<AuthPage />} />
-      <Route path="/auth/:method/:accountType" element={<AuthPage />} />
-      <Route path="/find-teachers" element={<TeacherSearchPage />} />
-      <Route path="/my-students" element={<StudentListPage />} />
-      <Route path="/teacher/:id" element={<TeacherDetailPage />} />
-      {user && <Route path="/inbox" element={<ChatInbox user={user} />} />}
-    </Routes>
-  )
-}
+import { UserProvider } from './context/UserContext';
 
 const container = document.getElementById('app') || document.createElement('div')
 container.id = 'app'
 const root = createRoot(container);
 root.render(
+  <React.StrictMode>
     <UserProvider>
       <Router>
-        <RouteList />
+        <Routes>
+          <Route path="/auth/:method" element={<AuthPage />} />
+          <Route path="/auth/:method/:accountType" element={<AuthPage />} />
+          <Route path="/find-teachers" element={<TeacherSearchPage />} />
+          <Route path="/my-students" element={<StudentListPage />} />
+          <Route path="/teacher/:id" element={<TeacherDetailPage />} />
+          <Route path="/inbox" element={<ChatInbox />} />
+        </Routes>
       </Router>
     </UserProvider>
+  </React.StrictMode>
 );
--- client/components/ChatInbox/ChatInbox.jsx
+++ client/components/ChatInbox/ChatInbox.jsx
@@ -1,83 +1,45 @@
-// import { useEffect, useRef, useState } from 'react';
-// import Talk from "talkjs";
-// import { useUserContext } from '../../context/UserContext';
-
-// export default function ChatInbox() {
-//   const inboxEl = useRef();
-//   const { user } = useUserContext();
-//   const [talkLoaded, setTalkLoaded] = useState(false);
-
-//   useEffect(() => {
-//     Talk.ready.then(() => setTalkLoaded(true));
-
-//     if (talkLoaded && user) {
-//       const currentUser = new Talk.User({
-//         id: user.id,
-//         name: `${user.firstName} ${user.lastName}`,
-//         photoUrl: user.imageUrl,
-//         role: 'default'
-//       });
-
-//       const session = new Talk.Session({
-//         appId: process.env.TALK_APP_ID,
-//         me: currentUser
-//       })
-
-//       const inbox = session.createInbox();
-//       inbox.mount(inboxEl.current);
-
-//       return () => session.destroy();
-//     }
-//   }, [talkLoaded, user]);
-
-//   return <div style={{height: '500px'}} className="inbox-container" ref={inboxEl}>Loading...</div>
-
-// }
-
-import { Component, Fragment } from 'react';
+import { useEffect, useRef, useState, Fragment } from 'react';
 import Talk from "talkjs";
+import { useUserContext } from '../../context/UserContext';
 
-class Messaging extends Component {
+export default function ChatInbox() {
+  const inboxEl = useRef();
+  const { user } = useUserContext();
+  const [talkLoaded, setTalkLoaded] = useState(false);
 
-    constructor(props) {
-        super(props);
-        this.inbox = undefined;
+  useEffect(() => {
+    Talk.ready.then(() => setTalkLoaded(true));
 
+    if (talkLoaded && user) {
+      const currentUser = new Talk.User({
+        id: user.id,
+        name: `${user.firstName} ${user.lastName}`,
+        photoUrl: user.imageUrl,
+        role: 'default'
+      });
+
+      if (!window.talkSession) {
+        window.talkSession = new Talk.Session({
+            appId: process.env.TALK_APP_ID,
+            me: currentUser
+        });
     }
 
-    componentDidMount() {
-        Talk.ready
-          .then(() => {
-            if (this.props.user) {
-              const me = new Talk.User({
-                id: this.props.user.id,
-                name: `${this.props.user.firstName} ${this.props.user.lastName}`,
-                photoUrl: this.props.user.imageUrl,
-                role: 'default'
-              });
-              if (!window.talkSession) {
-                  window.talkSession = new Talk.Session({
-                      appId: process.env.TALK_APP_ID,
-                      me: me
-                  });
-              }
-          
-              this.inbox = window.talkSession.createInbox();
-              this.inbox.mount(this.container);
+      const session = new Talk.Session({
+        appId: process.env.TALK_APP_ID,
+        me: currentUser
+      })
 
-            }
+      const inbox = session.createInbox();
+      inbox.mount(inboxEl.current);
 
-          })
-          .catch(e => console.error(e));
+      return () => session.destroy();
     }
+  }, [talkLoaded, user]);
 
-    render() {
-        return (
-            <Fragment>
-                <div style={{height: '500px'}} className="inbox-container" ref={c => this.container = c}>Loading...</div>
-            </Fragment>
-        );
-    }
-  }
-  
-  export default Messaging;
+  return (
+  <Fragment>
+    <div style={{height: '500px'}} className="inbox-container" ref={inboxEl}>Loading...</div>
+  </Fragment>
+  )
+}
