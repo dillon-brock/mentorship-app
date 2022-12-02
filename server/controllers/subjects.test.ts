e4f8ae1970e7dab51c0b959ef5285e61b2f7c1df
Dillon Brock <mdillon1297@gmail.com>
2022-12-02 11:46:08 -0800

    passing test for POST /subjects

--- server/controllers/subjects.test.ts
+++ server/controllers/subjects.test.ts
@@ -27,11 +27,18 @@ const testTeacher = {
   state: 'OR'
 }
 
+const testSubject = {
+  subject: 'Cooking',
+  minPrice: 30,
+  maxPrice: 50,
+  lessonType: 'Any'
+}
+
 describe('subjects controller', () => {
   beforeEach(() => {
     return setupDb();
   })
-  it('serves a list of subjects corresponding to teacher from query params', async () => {
+  it('serves a list of subjects corresponding to teacher from query params at GET /subjects/:teacherId', async () => {
     const agent = request.agent(app);
     const teacherRes = await agent.post('/teachers').send(testTeacher);
     const res = await agent.get(`/subjects/${teacherRes.body.teacher.id}`);
@@ -44,5 +51,12 @@ describe('subjects controller', () => {
       maxPrice: expect.any(Number),
       lessonType: expect.any(String)
     }))
+  })
+  it('adds a new subject corresponding to a teacher at POST /subjects', async () => {
+    const agent = request.agent(app);
+    const teacherRes = await agent.post('/teachers').send(testTeacher);
+    const res = await agent.post('/subjects').send(testSubject);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(expect.objectContaining({ ...testSubject }));
   })
 })
