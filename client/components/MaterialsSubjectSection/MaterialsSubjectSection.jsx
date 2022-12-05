a525ed845bd451aa70860c3bdc9bf1d84dd19406
Dillon Brock <mdillon1297@gmail.com>
2022-12-04 16:07:28 -0800

    converted subject section to dropdown

--- client/components/MaterialsSubjectSection/MaterialsSubjectSection.jsx
+++ client/components/MaterialsSubjectSection/MaterialsSubjectSection.jsx
@@ -1,34 +1,55 @@
-import { Image } from "react-bootstrap";
-
+import { useState } from "react";
+import { Button, Image } from "react-bootstrap";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import FileMaterial from "../FileMaterial/FileMaterial";
 import LinkMaterial from "../LinkMaterial/LinkMaterial";
+import styles from './materialSubjectSection.module.css';
 
 export default function MaterialsSubjectSection({ subject, teachingMaterials, setTeachingMaterials, subjects }) {
   const files = teachingMaterials.filter(material => material.type === 'file' && material.subjectId == subject.id );
   const links = teachingMaterials.filter(material => material.type === 'link' && material.subjectId == subject.id );
-  console.log(subject);
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <section>
-      <h3>{subject.subject}</h3>
-      <h5>Uploaded Files</h5>
-      {files.length > 0 ?
+    <section className={styles.section}>
+      <div className={styles.titleContainer}>
+        <h3>{subject.subject}</h3>
+        {expanded ?
+          <Button className={styles.chevronButton} onClick={() => setExpanded(false)}>
+            <FaChevronUp />
+          </Button>
+          :
+          <Button className={styles.chevronButton} onClick={() => setExpanded(true)}>
+            <FaChevronDown />
+          </Button>
+        }
+      </div>
+      <hr className={styles.hr} />
+      {expanded &&
       <>
-        {files.map(file => (
-          <FileMaterial key={file.id} {...file} setTeachingMaterials={setTeachingMaterials} subjects={subjects} />
-        ))}
-      </>
-      :
-      <>
-        <h6>{`You currently have no uploaded files for the subject ${subject.subject}.`}</h6>
-      </>
-      }
-      <h5>Links</h5>
-      {links.length > 0 ?
-        <ul>
-          {links.map(link => <LinkMaterial key={link.id} {...link} setTeachingMaterials={setTeachingMaterials} subjects={subjects} />)}
-        </ul>
+      <div className={styles.content}>
+        <h4 className={styles.subtitle}>Files</h4>
+        {files.length > 0 ?
+        <>
+          {files.map(file => (
+            <FileMaterial key={file.id} {...file} setTeachingMaterials={setTeachingMaterials} subjects={subjects} />
+          ))}
+        </>
         :
-        <h6>{`You currently have no links for the subject ${subject.subject}.`}</h6>
+        <>
+          <h5>You currently have no uploaded files.</h5>
+        </>
+        }
+        <h4 className={styles.subtitle}>Links</h4>
+        {links.length > 0 ?
+          <ul>
+            {links.map(link => <LinkMaterial key={link.id} {...link} setTeachingMaterials={setTeachingMaterials} subjects={subjects} />)}
+          </ul>
+          :
+          <h5>You currently have no saved links.</h5>
+        }
+      </div>
+      </>
       }
     </section>
   )
