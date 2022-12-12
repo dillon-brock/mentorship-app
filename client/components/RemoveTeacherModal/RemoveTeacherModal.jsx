6c278b5a0a269cc25b1db89609b9ee49145e2635
Dillon Brock <mdillon1297@gmail.com>
2022-12-11 20:54:53 -0800

    styling done for remove teacher modal

--- client/components/RemoveTeacherModal/RemoveTeacherModal.jsx
+++ client/components/RemoveTeacherModal/RemoveTeacherModal.jsx
@@ -1,4 +1,6 @@
 import { Button, Modal } from "react-bootstrap";
+import styles from './removeTeacherModal.module.css';
+import globalStyles from '../../global.module.css';
 
 export default function RemoveTeacherModal({ handleRemoveTeacher, userWantsToRemoveTeacher, setUserWantsToRemoveTeacher, firstName }) {
 
@@ -6,18 +8,18 @@ export default function RemoveTeacherModal({ handleRemoveTeacher, userWantsToRem
 
   return (
     <>
-      <Button onClick={() => setUserWantsToRemoveTeacher(true)}>Disconnect</Button>
-      <Modal show={userWantsToRemoveTeacher} onHide={handleClose}>
+      <Button className={styles.openButton} onClick={() => setUserWantsToRemoveTeacher(true)}>Disconnect</Button>
+      <Modal className={styles.modal} show={userWantsToRemoveTeacher} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Are you sure you want to disconnect from {firstName}?</Modal.Title>
+          <Modal.Title className={styles.title}>Are you sure you want to disconnect from {firstName}?</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+        <Modal.Body className={styles.body}>
           You will no longer have access to {firstName}&apos;s teaching materials. If you change your mind, you can add {firstName} as an instructor again, but the request will need to be re-approved.
         </Modal.Body>
         <Modal.Footer>
-          <div>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleRemoveTeacher}>Disconnect</Button>
+          <div className={styles.buttonContainer}>
+            <Button className={globalStyles.cancelButton} onClick={handleClose}>Cancel</Button>
+            <Button className={styles.removeButton} onClick={handleRemoveTeacher}>Disconnect</Button>
           </div>
         </Modal.Footer>
       </Modal>
