13085455b9a76f662560e8c169943275fd199112
Dillon Brock <mdillon1297@gmail.com>
2022-12-15 17:18:07 -0800

    cleaned up code in useAllTeachers

--- client/hooks/useAllTeachers.js
+++ client/hooks/useAllTeachers.js
@@ -11,37 +11,22 @@ export function useAllTeachers(subject, zipCode, lessonType, minPrice, maxPrice,
   useEffect(() => {
     const fetchTeachers = async () => {
       setLoading(true);
-      const data = await getTeachers(subject, lessonType, minPrice, maxPrice);
+      let data = await getTeachers(subject, lessonType, minPrice, maxPrice);
       if (zipCode && Number(radius)) {
         const zipCodesInRadius = await getZipCodesInRadius({ zipCode, radius });
         const errorStatus = zipCodesInRadius.error_code;
         if (errorStatus === 404) {
-          const pagedData = data.slice((page - 1) * pageLength, page * pageLength);
-          setTeachers(pagedData);
-          setTotalPages(Math.ceil(data.length / pageLength));
           setErrorMessage('Please enter a valid zip code to find instructors near you.');
-          setLoading(false);
         } else {
-          const filteredData = data.filter(t => zipCodesInRadius.zip_codes.includes(t.zipCode));
-          const pagedData = filteredData.slice((page - 1) * pageLength, page * pageLength);
-          setTeachers(pagedData);
-          setTotalPages(Math.ceil(filteredData.length / pageLength));
-          setLoading(false);
+          data = data.filter(t => zipCodesInRadius.zip_codes.includes(t.zipCode));
         }
-      } else {
-        if (zipCode && radius === '0') {
-          const filteredData = data.filter(t => t.zipCode === zipCode);
-          const pagedData = filteredData.slice((page - 1) * pageLength, page * pageLength);
-          setTeachers(pagedData);
-          setTotalPages(Math.ceil(filteredData.length / pageLength));
-          setLoading(false);
-        } else {
-          setTotalPages(Math.ceil(data.length / pageLength));
-          const pagedData = data.slice((page - 1) * pageLength, page * pageLength);
-          setTeachers(pagedData);
-          setLoading(false);
-        }
+      } else if (zipCode && radius === '0') {
+        data = data.filter(t => t.zipCode === zipCode);
       }
+      const pagedData = data.slice((page - 1) * pageLength, page * pageLength);
+      setTeachers(pagedData);
+      setTotalPages(Math.ceil(data.length / pageLength));
+      setLoading(false);
     }
     fetchTeachers();
   }, [subject, zipCode, radius, page, pageLength, lessonType, minPrice, maxPrice])
