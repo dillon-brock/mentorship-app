0cdfeeda5092c20ee00ea361e2e19f5d855a16fd
Dillon Brock <mdillon1297@gmail.com>
2023-04-26 20:30:57 -0700

    converted MaterialUploadModal, fixed broken className

--- client/components/MaterialUploadModal/MaterialUploadModal.tsx
+++ client/components/MaterialUploadModal/MaterialUploadModal.tsx
@@ -1,21 +1,30 @@
 import { Modal, Tab, Tabs } from "react-bootstrap";
 import MaterialFileUploadForm from "../MaterialFileUploadForm/MaterialFileUploadForm";
 import MaterialLinkUploadForm from "../MaterialLinkUploadForm/MaterialLinkUploadForm";
-import styles from './materialUploadModal.module.css';
+import { Dispatch, SetStateAction } from "react";
+import TeachingMaterial from "../../../server/models/TeachingMaterial";
+import Subject from "../../../server/models/Subject";
+
+type Props = {
+  showUploadModal: boolean;
+  setShowUploadModal: Dispatch<SetStateAction<boolean>>;
+  setTeachingMaterials: Dispatch<SetStateAction<TeachingMaterial[]>>;
+  subjects: Subject[];
+}
 
 export default function MaterialUploadModal({ 
   showUploadModal, 
   setShowUploadModal, 
   setTeachingMaterials, 
   subjects 
-}) {
+}: Props) {
   
   const handleClose = () => setShowUploadModal(false);
 
   return (
     <Modal show={showUploadModal} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title className={styles.modalTitle}>Upload Teaching Materials</Modal.Title>
+        <Modal.Title>Upload Teaching Materials</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Tabs
